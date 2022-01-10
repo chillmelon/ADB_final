988c6b786a68ae15c9b056df37c307cfdf64ceb8
ricky42613 <ricky42613@gmail.com>
2022-01-10 19:12:04 +0800

    add sidebar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('side/search');
+  res.render('home/index');
 });
 
 module.exports = router;
