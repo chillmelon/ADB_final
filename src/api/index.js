aebb1c9217e37d3eb5b55d3b8a203249090489ce
cityrain72 <sskw1682@gmail.com>
2022-01-15 21:38:29 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 const house_controller = require("./routers/house");
+const territory_controller = require("./routers/territory")
 
 console.log("api loaded");
 
 module.exports = (app) => {
 	app.use("/api/house", house_controller());
+	app.use("/api/territory", territory_controller());
 };
