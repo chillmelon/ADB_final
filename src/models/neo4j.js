07b319989e83e88b81f065759bdee60040bed201
ricky42613 <ricky42613@gmail.com>
2022-01-16 21:26:21 +0800

    show graph with api

--- src/models/neo4j.js
+++ src/models/neo4j.js
@@ -2,7 +2,7 @@ const neo4j = require("neo4j-driver");
 
 //const dbConfig = require("../config/db.js")
 
-driver = neo4j.driver('bolt://192.168.11.18:7687', neo4j.auth.basic('neo4j', '1234'));
+driver = neo4j.driver('bolt://172.20.10.9:7687', neo4j.auth.basic('neo4j', '1234'));
 
 /*const pool = new Pool({
 	host: dbConfig.host,
